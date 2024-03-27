21143a94979610c600f47cedb9e45f720cefb128
Daniel Vargas <22326866+DanielVargasIT@users.noreply.github.com>
2024-03-27 11:22:12 -0500

    third commit

---
+++ 1st-terraform-kms/main.tf
@@ -0,0 +1,88 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_kms_key" "main" {
+  description             = "aws-kms-cmk-rds"
+  deletion_window_in_days = 7
+  key_usage               = "ENCRYPT_DECRYPT"
+  policy                  = <<EOF
+{
+    "Id": "key-policy",
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Enable IAM User Permissions",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::000000000000:root"
+            },
+            "Action": "kms:*",
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow access for Key Administrators",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::000000000000:user/dvit-demo"
+            },
+            "Action": [
+                "kms:Create*",
+                "kms:Describe*",
+                "kms:Enable*",
+                "kms:List*",
+                "kms:Put*",
+                "kms:Update*",
+                "kms:Revoke*",
+                "kms:Disable*",
+                "kms:Get*",
+                "kms:Delete*",
+                "kms:TagResource",
+                "kms:UntagResource",
+                "kms:ScheduleKeyDeletion",
+                "kms:CancelKeyDeletion"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow use of the key",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::000000000000:role/aws-service-role/rds.amazonaws.com/AWSServiceRoleForRDS"
+            },
+            "Action": [
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:DescribeKey"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "Allow attachment of persistent resources",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::000000000000:role/aws-service-role/rds.amazonaws.com/AWSServiceRoleForRDS"
+            },
+            "Action": [
+                "kms:CreateGrant",
+                "kms:ListGrants",
+                "kms:RevokeGrant"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "Bool": {
+                    "kms:GrantIsForAWSResource": "true"
+                }
+            }
+        }
+    ]
+}
+EOF
+}
+
+output "arn" {
+  description = "The ARN of the key"
+  value       = aws_kms_key.main.arn
+}
